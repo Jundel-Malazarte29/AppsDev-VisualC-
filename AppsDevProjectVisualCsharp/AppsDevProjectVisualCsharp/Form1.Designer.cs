bad6819ef06ef28b2a95804dffb20dc2e09442bd
Jundel Malazarte <93628737+Jundel-Malazarte29@users.noreply.github.com>
2023-05-08 11:33:52 +0800

    Add files via upload

---
+++ AppsDevProjectVisualCsharp/AppsDevProjectVisualCsharp/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace AppsDevProjectVisualCsharp
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Homepage));
+            this.genzBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // genzBTN
+            // 
+            this.genzBTN.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.genzBTN.BackColor = System.Drawing.Color.Transparent;
+            this.genzBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genzBTN.ForeColor = System.Drawing.Color.OrangeRed;
+            this.genzBTN.Location = new System.Drawing.Point(252, 354);
+            this.genzBTN.Name = "genzBTN";
+            this.genzBTN.Size = new System.Drawing.Size(110, 40);
+            this.genzBTN.TabIndex = 0;
+            this.genzBTN.Text = "TRY GEN-Z BUBBLE TEA";
+            this.genzBTN.UseVisualStyleBackColor = false;
+            this.genzBTN.Click += new System.EventHandler(this.genzBTN_Click);
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(616, 428);
+            this.Controls.Add(this.genzBTN);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.Name = "Homepage";
+            this.Text = "Gen-Z Home page";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genzBTN;
+    }
+}
+
---
+++ AppsDevProjectVisualCsharp/AppsDevProjectVisualCsharp/Loginpage.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AppsDevProjectVisualCsharp
+{
+    partial class Loginpage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loginpage));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Signin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(132, 172);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 228);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 25);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Signin
+            // 
+            this.Signin.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Signin.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Signin.Location = new System.Drawing.Point(162, 278);
+            this.Signin.Name = "Signin";
+            this.Signin.Size = new System.Drawing.Size(50, 27);
+            this.Signin.TabIndex = 3;
+            this.Signin.Text = "Sign in";
+            this.Signin.UseVisualStyleBackColor = true;
+            // 
+            // Loginpage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(628, 435);
+            this.Controls.Add(this.Signin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Loginpage";
+            this.Text = "Loginpage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Signin;
+
+    }
+}
